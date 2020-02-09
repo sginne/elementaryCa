5307b80b3e6435ff6d17595c6e2e740f7f9f7d61
sginne <sigin@norsula.com>
2020-02-09 10:21:44 +0200

    changed gridname

--- elementaryCa.js
+++ elementaryCa.js
@@ -40,6 +40,6 @@ var gridOptions = {
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
-  var gridDiv = document.querySelector('#myGrid');
+  var gridDiv = document.querySelector('#caGrid');
   new agGrid.Grid(gridDiv, gridOptions);
 });
